6f8bc32a9b485a36c457703064bcc5128b6ea629
wr0ng0ne <official.tasnimhaider@gmail.com>
2024-10-16 06:35:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let inputBx = document.querySelector('#inputBx');
+let list = document.querySelector('#list');
+let addBtn = document.querySelector('#addBtn');
+
+
+let addItem = () => {
+    let inputValue = inputBx.value.trim();
+    if (inputValue !== ""){
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${inputValue} <i></i>`;
+    
+    listItem.addEventListener("click", function() {
+        this.classList.toggle('done');
+    });
+
+    listItem.querySelector("i").addEventListener("click", function() {
+        listItem.remove();
+    });
+
+    list.appendChild(listItem);
+    inputBx.value = "";
+    }
+};
+
+inputBx.addEventListener("keyup", function(event) {
+    if (event.key === "Enter")
+        addItem();
+});
+
+addBtn.addEventListener("click",addItem);
